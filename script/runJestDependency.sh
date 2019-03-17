c9361ce671d4f8f405ecc37b6a3b34b0a5fde343
Emmanuel Haguet <emmanuel.haguet@laposte.net>
2019-03-17 12:13:32 +0100

    runJestDependency: exclude jsvat and avoid package-lock notice

--- script/runJestDependency.sh
+++ script/runJestDependency.sh
@@ -9,9 +9,10 @@ npm list --depth=0 --parseable=true --only=prod | tail -n +2 > listDependency.tx
 while read path; do
     cd "$path" &&
     echo "$path" &&
-    cmd=$(node -p "var scripts = require('./package.json').scripts || {test: ''}; var test = scripts.test; (test == 'mocha --watch' || test.includes('exit') ) ? 'mocha': test") &&
+    cmd=$(node -p "if (require('./package.json').name == 'jsvat') { return ''} var scripts = require('./package.json').scripts || {test: ''}; var test = scripts.test; (test == 'mocha --watch' || test.includes('exit') ) ? 'mocha': test") &&
     echo "$cmd" &&
     npm install --production &&    # warning
+    rm "package-lock.json" &&
     eval $cmd #npm test
 
     #testcommand=$(node -p "(require('./package.json').scripts || {test: ''}).test")
